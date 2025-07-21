0e04f08e92ef243066fd0349b6f3b20497f259b4
MrCopperrrr <ntphat0802@gmail.com>
2025-07-21 10:06:12 +0700

    DLL destructor

--- TextBuffer.cpp
+++ TextBuffer.cpp
@@ -11,10 +11,15 @@ DoublyLinkedList<T>::DoublyLinkedList() {
 
 template <typename T>
 DoublyLinkedList<T>::~DoublyLinkedList() {
-    // TODO
+    Node* curr = head;
+    while(curr != nullptr){
+        Node *nodeNext = curr->next;
+        delete curr;
+        curr = nodeNext;
+    }
 }
 
-// TODO: implement other methods of DoublyLinkedList
+
 
 // ----------------- TextBuffer -----------------
 TextBuffer::TextBuffer() {
@@ -25,7 +30,7 @@ TextBuffer::~TextBuffer() {
     // TODO
 }
 
-// TODO: implement other methods of TextBuffer
+
 
 // ----------------- HistoryManager -----------------
 TextBuffer::HistoryManager::HistoryManager() {
@@ -36,7 +41,6 @@ TextBuffer::HistoryManager::~HistoryManager() {
     // TODO
 }
 
-//TODO: implement other methods of HistoryManager
 
 
 // Explicit template instantiation for char, string, int, double, float, and Point
