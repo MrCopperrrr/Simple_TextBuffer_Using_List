64707b4d93077455419247aa87418959f1f6b9ab
MrCopperrrr <ntphat0802@gmail.com>
2025-07-23 11:12:44 +0700

    TextBuffer.h

--- TextBuffer.h
+++ TextBuffer.h
@@ -3,8 +3,14 @@
 
 #include "main.h"
 
+//forward declare
+class TextBuffer;
+class HistoryManager;
+
+
 template <typename T>
 class DoublyLinkedList {
+private:
     struct Node {
         T data;
         Node *next;
@@ -30,15 +36,55 @@ public:
     bool contains(T item) const;
     int size() const;
     void reverse();
+    bool empty() const;
     string toString(string (*convert2str)(T&) = 0) const;
 };
 
 class TextBuffer {
-private:
-    DoublyLinkedList<char> buffer; 
+public:
+    class HistoryManager {
+    private:
+        struct ActionRecord {
+            string action;
+            int cursorPos;
+            char ch;
+
+            ActionRecord(string action, int cursorPos, char ch)
+                : action(action), cursorPos(cursorPos), ch(ch) {}
+
+            ActionRecord() : action(""), cursorPos(0), ch('\0') {} // default
+        };
+
+        DoublyLinkedList<ActionRecord> history;
+
+    public:
+        HistoryManager();
+        ~HistoryManager();
+
+        void addAction(const string &actionName, int cursorPos, char c);
+        void printHistory() const;
+        int size() const;
+    };
+
+    DoublyLinkedList<char> buffer;
     int cursorPos;
 
-    // TODO: may provide some attributes
+    enum ActionType { INSERT, DELETE, MOVE };
+    struct Action {
+        ActionType type;
+        int pos;
+        char data;
+
+        Action(ActionType type, int pos, char data = '\0')
+            : type(type), pos(pos), data(data) {}
+
+        Action() : type(INSERT), pos(0), data('\0') {} //default
+    };
+
+    DoublyLinkedList<Action> undoStack;
+    DoublyLinkedList<Action> redoStack;
+
+    HistoryManager history; 
 
 public:
     TextBuffer();
@@ -58,18 +104,7 @@ public:
     void undo();
     void redo();
 
-public:
-    class HistoryManager {
-        // TODO: may provide some attributes
 
-    public:
-        HistoryManager();
-        ~HistoryManager();
-
-        void addAction(const string &actionName, int cursorPos, char c);
-        void printHistory() const;
-        int size() const;
-    };
 };
 
 #endif // __TEXT_BUFFER_H__
